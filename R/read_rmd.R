e5b0dc77dc060b5e379682bf868bb885f5c38073
Miguel Alvarez <kamapu78@gmail.com>
2020-12-12 14:25:17 +0100

    Version with vignette

--- R/read_rmd.R
+++ R/read_rmd.R
@@ -1,13 +1,16 @@
 #' @name read_rmd
+#' @rdname read_rmd
 #' 
 #' @title Read R-markdown Documents
 #' 
 #' @description 
-#' This function reads Rmd files into character vectors pasting a line break
-#' at the end of each line.
+#' The function `read_rmd()` reads Rmd files into character vectors pasting a
+#' line break at the end of each line.
+#' The function `txt_body()` will do the same to one or more character vectors.
 #' 
 #' @param file Character value indicating the path and the name to the Rmd file.
-#' @param ... Arguments passed to [readLines()].
+#' @param ... Arguments passed by `read_rmd()` to [readLines()].
+#'     In `txt_body()` they are character values passed to `c()`.
 #' @param skip_head Logical value indicating whether the yaml head should be
 #'     skip or not.
 #' 
@@ -19,11 +22,10 @@
 #' Doc <- read_rmd(file.path(path.package("yamlme"), "taxlistjourney.Rmd"))
 #' }
 #' 
-#' @export 
-#' read_rmd
+#' @export read_rmd
 #' 
 read_rmd <- function(file, ..., skip_head=TRUE) {
-	file <- paste0(readLines(file, ...), "\n")
+	file <- txt_body(readLines(file, ...))
 	if(skip_head) {
 		if(substr(file[1], 1, 3) != "---")
 			message("Rmd file seems to be headless") else {
@@ -32,4 +34,13 @@ read_rmd <- function(file, ..., skip_head=TRUE) {
 		}
 	}
 	return(file)
+}
+
+#' @aliases txt_body
+#' @rdname read_rmd
+#' 
+#' @export txt_body
+#' 
+txt_body <- function(...) {
+	return(paste0(c(...), "\n"))
 }

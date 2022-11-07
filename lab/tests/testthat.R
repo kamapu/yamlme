be5aaebe7934aa141279f457ec0b3c213ecf9a14
Miguel Alvarez <kamapu78@gmail.com>
2022-11-07 20:01:30 +0100

    Deleted lab folder

--- lab/tests/testthat.R
+++
@@ -1,8 +0,0 @@
-# browseURL("https://kbroman.org/pkg_primer/pages/tests.html")
-# browseURL(file.path("https://walczak.org/2017/06",
-# 		"how-to-add-code-coverage-codecov-to-your-r-package/"))
-
-library(testthat)
-library(yamlme)
-
-test_check("yamlme")
--- lab/tests/testthat/test-read_rmd.R
+++
@@ -1,6 +0,0 @@
-context("reading r-markdown")
-
-test_that("read_rmd is working", {
-  file <- file.path(path.package("yamlme"), "taxlistjourney.Rmd")
-  expect_is(read_rmd(file), "rmd_doc")
-})
